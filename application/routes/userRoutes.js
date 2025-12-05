d70ea1fcbb9c2dcd5383f539cdc14de769e97b45
ck496 <chriskurian24@gmail.com>
2025-12-04 20:37:14 -0700

    feat: added new routes, updated app.js and added new npm configs

--- application/routes/userRoutes.js
+++ application/routes/userRoutes.js
@@ -1,10 +1,28 @@
 import express from "express";
 
-import { registerUser, getUser } from "../controllers/userController.js";
+import {
+  registerUser,
+  getUser,
+  updateUserRole,
+  deactivateUser,
+  userExists,
+} from "../controllers/userController.js";
 
 const router = express.Router();
 
+// Registr new user
 router.post("/register", registerUser);
+
+// Check if user exists
+router.get("/exists/:id", userExists);
+
+// Get user by ID
 router.get("/:id", getUser);
+
+// Update user role
+router.put("/:id/role", updateUserRole);
+
+// Deactivate user (soft delete)
+router.delete("/:id", deactivateUser);
 
 export default router;
